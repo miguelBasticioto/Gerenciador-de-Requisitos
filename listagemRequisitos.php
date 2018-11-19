e816675faa6e542370ff3488bdedfa3537a320ca
miguelBasticioto <miguel_basticioto@hotmail.com>
2018-11-19 19:02:31 -0300

    Inicio do layout da tabela de requisitos

--- listagemRequisitos.php
+++ listagemRequisitos.php
@@ -18,11 +18,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     <title>Listagem de Requisitos</title>
 </head>
 <body>
-    <table>
+    <table class = "w3-table w3" style = "background-color: #fafafa">
         <?php
             if ($result->num_rows > 0) {
                 // output data of each row
@@ -40,6 +41,7 @@
             }
         ?>
     </table>
+
 </body>
 </html>
 <?php
